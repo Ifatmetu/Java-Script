f2aa2465c06bd129cb2ca510564b2e5ec8345e5d
Ifat Metu <ifatjahan305@gmail.com>
2026-03-18 08:26:13 +0600

    lesson2:variable(var,let,const)

---
+++ lesson2/index.js
@@ -0,0 +1,21 @@
+//variable using var keyword
+        var name1="Java\n"
+        console.log(name1 )
+        var name1="JavaScript"
+        console.log(name1)
+        console.log("\n\n")
+
+
+
+      //variable using let keyword 
+        let name2="C language\n"
+        console.log(name2 )
+       //let name2="Itu"//error redeclar not allow
+        name2="C++ language"
+        console.log(name2) 
+
+
+        //variable using const keyword 
+        const name3="System\n"
+        console.log(name3 )
+        //name3="Design"//error can't change the value of const variable
