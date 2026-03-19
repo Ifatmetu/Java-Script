a1f0f2d4fafeb993d1c9be618d6093e9300fd058
Ifat Metu <ifatjahan305@gmail.com>
2026-03-19 07:16:52 +0600

    Challange

--- Challange1/index.js
+++ Challange1/index.js
@@ -34,3 +34,19 @@ let expenses=Number(prompt("\nEnter your expenses amount:"));
 console.log("\nUser name:"+UserName);
 console.log("Income Amount"+income);
 console.log("Expenses Amount:"+expenses);
+
+
+//Personal budget tracker Challange(Phase 3-String concatenation and methods)
+/*
+Challange Requirement:
+Format the output following way:
+Personal Budget Tracker
+User:IFAT JAHAN METU
+Total Income:$3750.25
+Total Expenses:$2500.75
+*/
+
+console.log("Personal Budget Tracker:")
+console.log("User:" + UserName.toUpperCase());
+console.log("Total Income: $" + income);
+console.log("Total Expenses: $" + expenses);
