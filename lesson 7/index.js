4de0b1ad4cf18e201149e28465bef3b8632ba4be
Ifat Metu <ifatjahan305@gmail.com>
2026-03-23 19:19:38 +0600

    Date object

---
+++ lesson 7/index.js
@@ -0,0 +1,21 @@
+//Date object
+var date=new Date();//for date
+console.log(date);
+
+var year=date.getFullYear();//for year
+console.log(year);
+
+var month=date.getMonth();//for Month
+console.log(month);
+
+var currentDate=date.getDate();//for Date
+console.log(currentDate);
+
+var currentDay=date.getDay();//for (saturday,sunday...)
+console.log(currentDay);
+
+var currentHour=date.getHours();//for hour
+console.log(currentHour);
+
+var currentMinutes=date.getMinutes();//for Minutes
+console.log(currentMinutes);
