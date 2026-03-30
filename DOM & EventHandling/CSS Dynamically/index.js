c8840666eb624bd2871c78feaa733b6d2493aee5
Ifat Metu <ifatjahan305@gmail.com>
2026-03-30 14:52:18 +0600

    DOM & EventHandling

---
+++ DOM & EventHandling/CSS Dynamically/index.js
@@ -0,0 +1,8 @@
+function addStyle(){
+    var myVar=document.querySelector("#paraid")
+    myVar.classList.add("para-style")
+}
+function removeStyle(){
+    var myVar=document.querySelector("#paraid")
+    myVar.classList.remove("para-style")
+}
